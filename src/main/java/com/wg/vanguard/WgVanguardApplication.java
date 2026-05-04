e693c341df32d21d72caae7b93a5036e0d14d709
Tabaxco <rodriguesgui48@gmail.com>
2026-05-04 15:04:45 -0300

    Starting a new college project

---
+++ src/main/java/com/wg/vanguard/WgVanguardApplication.java
@@ -0,0 +1,13 @@
+package com.wg.vanguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgVanguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WgVanguardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wg/vanguard/WgVanguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wg.vanguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgVanguardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
